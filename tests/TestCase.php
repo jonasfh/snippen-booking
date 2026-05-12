cc370c7884e1dc72200ebd8f7b16ded8dd38a010
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2026-05-12 22:01:39 +0000

    test: add composer.json and PHPUnit configuration
    - Add composer.json with PHPUnit and dev dependencies
    - Create phpunit.xml.dist with Unit and Integration testsuites
    - Add tests/bootstrap.php to load plugin autoloader
    - Create TestCase base class for test inheritance
    - Setup test directory structure

---
+++ tests/TestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SnippenBooking\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Base test case class for all tests
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase {
+
+    /**
+     * Set up test environment
+     */
+    protected function setUp(): void {
+        parent::setUp();
+    }
+
+    /**
+     * Tear down test environment
+     */
+    protected function tearDown(): void {
+        parent::tearDown();
+    }
+}
