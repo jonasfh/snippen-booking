dd4f1b9a2a229929dbf318f8008c9e17c03a645e
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2026-05-14 20:45:55 +0000

    Bump version

--- src/wp-content/plugins/booking-plugin/booking-plugin.php
+++ src/wp-content/plugins/booking-plugin/booking-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Snippen Booking
  * Description: Booking plugin for Snippen community house.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Snippen
  * Text Domain: snippen-booking
  * License: GPLv2 or later
