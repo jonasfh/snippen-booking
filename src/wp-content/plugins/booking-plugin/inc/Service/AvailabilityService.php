2a477b6408e9c1b0f9a6cb828791a2df309bcca5
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2026-05-14 16:42:03 +0000

    More robust overlap checking

--- src/wp-content/plugins/booking-plugin/inc/Service/AvailabilityService.php
+++ src/wp-content/plugins/booking-plugin/inc/Service/AvailabilityService.php
@@ -48,7 +48,15 @@ class AvailabilityService {
         foreach ($existing_bookings as $booking) {
             $booked_window = $this->calculateWindow($booking->booking_date, $booking->start_time, $booking->end_time, $booking->cleanup_hours);
             
-            if ($this->isOverlapping($proposed_window, $booked_window)) {
+            $overlap = $this->isOverlapping($proposed_window, $booked_window);
+            if ($overlap) {
+                error_log(sprintf(
+                    "OVERLAP DETECTED: Proposed [%s - %s] vs Booked [%s - %s]",
+                    $proposed_window['start']->format('Y-m-d H:i:s'),
+                    $proposed_window['end']->format('Y-m-d H:i:s'),
+                    $booked_window['start']->format('Y-m-d H:i:s'),
+                    $booked_window['end']->format('Y-m-d H:i:s')
+                ));
                 return false;
             }
         }
@@ -106,7 +114,17 @@ class AvailabilityService {
                 foreach ($existing_bookings as $booking) {
                     $booked_window = $this->calculateWindow($booking->booking_date, $booking->start_time, $booking->end_time, $booking->cleanup_hours);
                     
-                    if ($this->isOverlapping($proposed_window, $booked_window)) {
+                    $overlap = $this->isOverlapping($proposed_window, $booked_window);
+                    if ($overlap) {
+                        error_log(sprintf(
+                            "OVERLAP DETECTED (bulk): Slot %d on %s. Proposed [%s - %s] vs Booked [%s - %s]",
+                            $slot->id,
+                            $date_str,
+                            $proposed_window['start']->format('Y-m-d H:i:s'),
+                            $proposed_window['end']->format('Y-m-d H:i:s'),
+                            $booked_window['start']->format('Y-m-d H:i:s'),
+                            $booked_window['end']->format('Y-m-d H:i:s')
+                        ));
                         $unavailable[$date_str][] = (int) $slot->id;
                         break; // Already unavailable, no need to check other bookings
                     }
