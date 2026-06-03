e4672756bb8c41a0847eaa4d545c4cacb812fadc
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2026-06-03 20:28:38 +0000

    (#107) Run local tests using database transactions to speed up execution

--- src/wp-content/plugins/booking-plugin/booking-plugin.php
+++ src/wp-content/plugins/booking-plugin/booking-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Snippen Booking
  * Description: Booking plugin for Snippen community house.
- * Version: 1.22.7
+ * Version: 1.22.8
  * Author: Snippen
  * Text Domain: snippen-booking
  * Domain Path: /languages
@@ -23,7 +23,7 @@ if ( ! function_exists( 'snippen_save_phone_number' ) ) {
 }
 
 // Define version constant
-define( 'SNIPPEN_BOOKING_VERSION', '1.22.7' );
+define( 'SNIPPEN_BOOKING_VERSION', '1.22.8' );
 // Load autoloader
 require_once __DIR__ . '/autoloader.php';
 
